0a6b280c8cd8ed64fda5dab5ad1234c0e931f7d3
atrovato <1839717+atrovato@users.noreply.github.com>
2021-03-10 16:43:04 +0100

    No auto device scan

--- steps/explorePeripheral.js
+++ steps/explorePeripheral.js
@@ -1,10 +1,9 @@
-const prompts = require('prompts');
 const { printDevice, uuidToMac } = require('../utils/printer');
 
 const explorePeripheral = async (executor) => {
   const { deviceUuid, service } = executor.result;
   executor.startLoading(`Exploring ${uuidToMac(deviceUuid)} device information...`);
-  await executor.bluetooth.scanDevice(deviceUuid);
+  // await executor.bluetooth.scanDevice(deviceUuid);
   const device = executor.bluetooth.getDiscoveredDevice(deviceUuid);
   executor.stopLoading();
   printDevice(device);
